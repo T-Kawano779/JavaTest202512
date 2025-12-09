e9c83d3821c2fda99d0e9fc92ef4024c150ef46a
T-Kawano779 <kawano@unity.with.co.jp>
2025-12-09 15:35:08 +0900

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void sayHello() {
+    public void sayHello() {
         System.out.println("Hello, World!");
         System.out.println("Hello, Java!");
         System.out.println("Hello, Git!");
